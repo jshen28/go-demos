aee9d6eb207ca2ad37f309f42e03a86378cfd291
ushen <yshxxsjt715@gmail.com>
2019-12-29 17:32:56 +0800

    add test01

---
+++ go.mod
@@ -0,0 +1,1 @@
+module github.com/jshen28/go-demos
---
+++ test01/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello World from test01")
+}
+
+func init() {
+	fmt.Println("init func is ran by default!")
+}
