e29c849af51c9ab62e2d071821ea18d53c53467a
shenjiatong <yshxxsjt715@gmail.com>
2020-11-09 15:21:17 +0800

    Construct an iface map

--- test04/main.go
+++ test04/main.go
@@ -6,6 +6,7 @@ import (
 )
 
 func localAddresses() {
+	iface_map := make(map[string][]string)
 	ifaces, err := net.Interfaces()
 	if err != nil {
 			fmt.Print(fmt.Errorf("localAddresses: %+v\n", err.Error()))
@@ -23,10 +24,18 @@ func localAddresses() {
 							fmt.Printf("%v : %s (%s)\n", i.Name, v, v.IP.DefaultMask())
 
 					case *net.IPNet:
-						
+							if _, ok := iface_map[i.Name]; !ok {
+									iface_map[i.Name] = []string {fmt.Sprintf("%s", v)}
+							} else {
+								iface_map[i.Name] = append(iface_map[i.Name], fmt.Sprintf("%s", v))
+							}
 							fmt.Printf("%v : %s [%v/%v]\n", i.Name, v, v.IP, v.Mask)
 					}
 			}
+	}
+
+	for iface, addrs := range iface_map {
+		fmt.Printf("%v, %v \n", iface, addrs)
 	}
 }
 
